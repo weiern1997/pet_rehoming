d7ab604e8d7692e4c0d8d91c331573d24ef39663
weiern1997 <weiern1997@gmail.com>
2021-12-08 17:20:57 +0800

    Update pets route
    Added CRUD routes with. Changes include:
    New page
    Adopt route
    Delete

--- routes/pets.js
+++ routes/pets.js
@@ -1,6 +1,8 @@
 var express = require('express');
 var router = express.Router();
 var multer = require('multer');
+var fs = require('fs');
+var path = require('path');
 
 const Pet = require('../models/pets');
 
@@ -18,7 +20,7 @@ var upload = multer({ storage: storage });
 
 //get all pets and render pets page
 router.get('/', function(req, res, next) {  
-  Pet.find(function(err, pets) {
+  Pet.find({adopted: false}, function(err, pets) {
     if (err) {
       console.log(err);
     } else {
@@ -31,6 +33,11 @@ router.get('/', function(req, res, next) {
   });
 });
 
+//Create a new pet and render new page
+router.get('/new', function(req, res, next) {
+  res.render('new', {title: 'New Pet', user: req.user});
+});
+
 //get a single pet and render show page
 router.get('/:id', function(req, res, next) {
   Pet.findById(req.params.id, function(err, pet) {
@@ -38,14 +45,50 @@ router.get('/:id', function(req, res, next) {
       console.log(err);
     } else {
       res.render('show', {
-        pet: pet
+        pet: pet,
+        title: pet.name,
+        user: req.user
       });
     }
   });
 });
 
+
+
+
+//Create a new pet and redirect to pets page
+router.post('/new', upload.single('image'), function(req, res, next) {
+  let pet = req.body
+  pet["img"] = {data: fs.readFileSync(path.join(__dirname, '../uploads/' + req.file.filename)),
+                 contentType: 'image/png'}
+  pet.adopted = false;
+  Pet.create(pet, function(err, pet) {
+    if (err) {
+      console.log(err);
+    } else {
+      res.redirect('/pets');
+    }
+  });
+});
+
+//Edit a pet and redirect to show page
+router.get('/:id/edit', function(req, res, next) {
+  Pet.findById(req.params.id, function(err, pet) {
+    if (err) {
+      console.log(err);
+    } else {
+      res.render('edit', {
+        pet: pet,
+        title: pet.name,
+        user: req.user
+      });
+    }
+  });
+});
+
+
 //Change adoption status to true
-router.put('/:id', function(req, res, next) {
+router.put('/:id/adopt', function(req, res, next) {
   Pet.findByIdAndUpdate(req.params.id, {adopted: true}, function(err, pet) {
     if (err) {
       console.log(err);
@@ -55,17 +98,9 @@ router.put('/:id', function(req, res, next) {
   });
 });
 
-//Create a new pet and render new page
-router.get('/new', function(req, res, next) {
-  res.render('new');
-});
-
-//Create a new pet and redirect to pets page
-router.post('/new', function(req, res, next) {
-  let pet = req.body
-  pet["image"]["data"] = fs.readFileSync(path.join(__dirname + '/uploads/' + req.file.filename))
-  pet["image"]["contentType"] = 'image/jpg'
-  Pet.create(req.body, function(err, pet) {
+//Delete a pet
+router.delete('/:id/delete', function(req, res, next) {
+  Pet.findByIdAndRemove(req.params.id, function(err, pet) {
     if (err) {
       console.log(err);
     } else {
@@ -73,6 +108,8 @@ router.post('/new', function(req, res, next) {
     }
   });
 });
+
+
 
 
 module.exports = router;
